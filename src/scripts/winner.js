1233636e23e0a2bf474c9c3cc318d444da5ff1f8
Avisek Pandit <simonp4466@gmail.com>
2022-09-21 17:41:08 -0400

    new changes

--- src/scripts/winner.js
+++ src/scripts/winner.js
@@ -6,20 +6,12 @@ export default class Winner{
         this.canvasDiv = document.getElementById('canvas-div');
         const ctx = canvas.getContext('2d');
 
-        console.log(winner);
-        ctx.clearRect(0,0,canvas.width, canvas.height);
-        ctx.fillStyle = "black";
-        ctx.fillRect(0,0,960,540);
-
         let WinnerText = document.createElement("Text");
         WinnerText.setAttribute("id", "WinnerText");
         this.canvasDiv.appendChild(WinnerText);
         WinnerText.innerHTML = winner;
 
         setTimeout(() => {
-            ctx.clearRect(0,0,canvas.width, canvas.height);
-            ctx.fillStyle = "black";
-            ctx.fillRect(0,0,960,540);
             WinnerText.remove();
             new View(canvas,ctx);
             delete this;
