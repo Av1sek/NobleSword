b433fa57344479962ca97691764a30f2eb70ea3e
Avisek Pandit <simonp4466@gmail.com>
2022-09-22 09:37:17 -0400

    change img src

--- src/scripts/platform.js
+++ src/scripts/platform.js
@@ -3,7 +3,7 @@ export default class Platform {
     constructor(ctx) {
         this.ctx = ctx;
         this.img = new Image();
-        this.img.src = "assets/backgrounds/NightBG/NightBG1.png";
+        this.img.src = "../../assets/backgrounds/NightBG/NightBG1.png";
         this.bgName = "NightBG"
         this.num = 1;
         this.frameCounter = 0;
@@ -19,7 +19,7 @@ export default class Platform {
         if (this.frameCounter % (this.stagger) === 0) {
             this.num++;
             if (this.num > (this.numFrames - 1)) {this.num = 1}
-            this.img.src = `assets/backgrounds/${this.bgName}/${this.bgName}${this.num}.png`
+            this.img.src = `../../assets/backgrounds/${this.bgName}/${this.bgName}${this.num}.png`
         }
     }
 
