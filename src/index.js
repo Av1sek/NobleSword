47c3d546e7076d51b99e37e4536dba74f6565d0d
Avisek Pandit <simonp4466@gmail.com>
2022-11-21 20:07:40 -0500

    fixed flashing

--- src/index.js
+++ src/index.js
@@ -6,5 +6,26 @@ document.addEventListener('DOMContentLoaded', () => {
     gameCanvas.width = 960;
     gameCanvas.height = 540;
 
+    for (let i = 1; i < 25; i++) {
+        let img = new Image();
+        img.src = `assets/backgrounds/NightBG/NightBG${i}.png`
+    }
+
+    preloadKnightAnims("Attack1", 6)
+    preloadKnightAnims("Attack2", 6)
+    preloadKnightAnims("Attack3", 8)
+    preloadKnightAnims("Block", 5)
+    preloadKnightAnims("BlockIdle", 8)
+    preloadKnightAnims("Hurt", 3)
+    preloadKnightAnims("Idle", 8)
+    preloadKnightAnims("Run", 10)
+
     new View(gameCanvas, ctx)
 })
+
+const preloadKnightAnims = (type, num) => {
+    for (let i = 0; i < num; i++) {
+        let img = new Image();
+        img.src = `assets/knight/${type}/HeroKnight_${type}_${i}.png`
+    }
+}
